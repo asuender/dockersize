32ca537e78e9280d63699eb742c6396fe46f547f
Andreas Sünder <andreas.suender@icloud.com>
2024-09-30 22:00:34 +0200

    Add base dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,44 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'dockersize',
-  description: 'A simple tool to get docker image sizes',
-}
+  title: "dockersize",
+  description: "A simple tool to get docker image sizes",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className='dark'>
-      <body className='flex items-center justify-center w-screen h-screen p-3'>
+    <html lang="en" className="dark dark:bg-slate-800">
+      <body className="flex items-center justify-center w-screen h-screen p-3">
         {children}
 
-        <footer className='fixed bottom-0 left-0 w-full text-center text-gray-500 py-2'>
+        <footer className="fixed bottom-0 left-0 w-full text-center text-gray-500 py-2">
           <p>
             Made by
-            <a href='https://github.com/asuender' className='text-blue-500 hover:text-blue-600'>
-              {' '}
+            <a
+              href="https://github.com/asuender"
+              className="text-blue-500 hover:text-blue-600 dark:text-blue-400"
+            >
+              {" "}
               asuender
             </a>
-            .
-            Created with
-            <a href='https://nextjs.org' className='text-blue-500 hover:text-blue-600'>
-              {' '}
-              Next.js
-              {' '}
+            . Created with
+            <a
+              href="https://nextjs.org"
+              className="text-blue-500 hover:text-blue-600 dark:text-blue-400"
+            >
+              {" "}
+              Next.js{" "}
             </a>
             and
-            <a href='https://vercel.com/' className='text-blue-500 hover:text-blue-600'>
-              {' '}
+            <a
+              href="https://vercel.com/"
+              className="text-blue-500 hover:text-blue-600 dark:text-blue-400"
+            >
+              {" "}
               Vercel
             </a>
             .
@@ -39,5 +46,5 @@ export default function RootLayout({
         </footer>
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import prettyBytes from 'pretty-bytes';
+import prettyBytes from "pretty-bytes";
 
-import { useState } from 'react';
-import Loading from './loading';
+import { useState } from "react";
+import Loading from "./loading";
 
 export default function Home() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [tags, setTags] = useState<any[] | null>([]);
   const [loading, setLoading] = useState(false);
 
@@ -14,67 +14,90 @@ export default function Home() {
     event.preventDefault();
     setLoading(true);
 
-    let repository = 'library', digest;
+    let repository = "library",
+      digest;
 
     if (/[a-z\d]{4,30}\/[a-zA-Z\d][a-zA-Z\d_.-]{0,254}/.test(input)) {
-      repository = input.split('/')[0];
-      digest = input.split('/')[1];
-    }
-
-    else if (/[a-zA-Z\d][a-zA-Z\d_.-]{0,254}/.test(input)) {
+      repository = input.split("/")[0];
+      digest = input.split("/")[1];
+    } else if (/[a-zA-Z\d][a-zA-Z\d_.-]{0,254}/.test(input)) {
       digest = input;
-    }
-
-    else {
+    } else {
       setTags(null);
     }
 
     const res = await fetch(`/api/images/${repository}/${digest}`);
     const data = await res.json();
 
-    setTags(data?.results)
+    setTags(data?.results);
     setLoading(false);
   }
 
   return (
     <main>
       <div>
-        <h1 className='text-4xl lg:text-6xl'>dockersize</h1>
+        <h1 className="text-4xl lg:text-6xl dark:text-white">dockersize</h1>
       </div>
 
-      <p className='text-gray-500'>A simple tool to get docker image sizes.</p>
+      <p className="text-gray-500 dark:text-slate-400">
+        A simple tool to get docker image sizes.
+      </p>
 
-      <form className='mt-4 mb-1' onSubmit={getImages}>
-        <input type='text' className='w-full rounded-md' value={input} onChange={(e) => setInput(e.target.value)} placeholder='e.g. ubuntu, mysql...' autoFocus required />
+      <form className="mt-4 mb-1" onSubmit={getImages}>
+        <input
+          type="text"
+          className="w-full rounded-md font-bold dark:text-slate-200 dark:placeholder:text-slate-300 dark:bg-slate-600"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="e.g. ubuntu, mysql..."
+          autoFocus
+          required
+        />
       </form>
 
-      <div className='output overflow-y-auto'>
-        {loading ? (<Loading />) : (tags?.map((tag) => (
-          <DockerTag key={tag.id} tag={tag} />
-        )))}
+      <div className="output overflow-y-auto">
+        {loading ? (
+          <Loading />
+        ) : (
+          tags?.map((tag) => <DockerTag key={tag.id} tag={tag} />)
+        )}
       </div>
     </main>
-  )
+  );
 }
 
 function DockerTag({ tag }: any) {
-  const { name, full_size, images } = tag || {}
+  const { name, full_size, images } = tag || {};
 
   return (
-    <div className='w-full py-2'>
-      <span className='font-bold'>{name}</span>
+    <div className="w-full py-2">
+      <span className="font-bold dark:text-slate-300 dark:underline">
+        {name}
+      </span>
 
-      <div className='flex flex-row justify-between text-xs uppercase'>
-        <span className='text-gray-500'>os/arch</span>
-        <span className='text-gray-500'>compressed size</span>
+      <div className="flex flex-row justify-between text-xs uppercase">
+        <span className="text-gray-500 dark:text-slate-400">os/arch</span>
+        <span className="text-gray-500 dark:text-slate-400">
+          compressed size
+        </span>
       </div>
 
       {images?.map((image: any) => (
-        <div key={image.digest} className='flex flex-row justify-between'>
-          <span>{image.os}/{image.architecture}</span>
-          <span>{prettyBytes(image.size, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+        <div
+          key={image.digest}
+          className="flex flex-row justify-between dark:text-slate-300"
+        >
+          <span>
+            {image.os}/{image.architecture}
+          </span>
+          <span>
+            {prettyBytes(image.size, {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
+          </span>
         </div>
       ))}
     </div>
-  )
+  );
 }
